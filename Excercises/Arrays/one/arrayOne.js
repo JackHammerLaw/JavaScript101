0e15a3ac86c57abd561138cc8306063ff915af5d
Dan Mcgillicuddy <danmcgillicuddy@Dans-MBP.fios-router.home>
2020-06-15 17:22:39 -0400

    minor: added more excercises

--- Excercises/Arrays/one/arrayOne.js
+++ Excercises/Arrays/one/arrayOne.js
@@ -1,15 +1,45 @@
 // Complete the tasks and return your final answer
 
+// Array Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+//EXAMPLE:
 // Remove the last element from elementsArray and return elements array
 function removeLastElement() {
-  const elementsArray = ["one", "two", "three", "four", "five"];
+  const numbersArray = ["one", "two", "three", "four", "five"];
+  //Wrie your code here
+  numbersArray.pop();
+
+  return numbersArray;
+}
+
+// Insert the element 'grape' in between banana and orange
+function insertElementAtIndex() {
+  const fruitsArray = ["apple", "banana", "orange"];
 
   //Wrie your code here
-  elementsArray.pop();
 
-  return elementsArray;
+  return fruitsArray;
+}
+
+function insertElementAtIndex() {
+  const fruitsArray = ["apple", "banana", "orange"];
+
+  //Wrie your code here
+
+  return fruitsArray;
+}
+
+// Remove 'mercedez' from carsArray
+function removeElementAtIndex() {
+  const carsArray = ["toyota", "honda", "mercedez", "hummer"];
+
+  //Wrie your code here
+
+  return carsArray;
 }
 
 module.exports = {
   removeLastElement,
+  insertElementAtIndex,
+  removeElementAtIndex,
 };
--- Excercises/Arrays/one/arrayOne.test.js
+++ Excercises/Arrays/one/arrayOne.test.js
@@ -1,4 +1,4 @@
-const { removeLastElement } = require("./arrayOne");
+const { removeLastElement, insertElementAtIndex } = require("./arrayOne");
 const expect = require("chai").expect;
 
 describe("Array One", function () {
@@ -6,5 +6,10 @@ describe("Array One", function () {
     const arr = removeLastElement();
     expect(arr).to.have.lengthOf(4);
     expect(arr).to.deep.equal(["one", "two", "three", "four"]);
+  });
+  it("Insert element in correct place", function () {
+    const arr = insertElementAtIndex();
+    expect(arr).to.have.lengthOf(4);
+    expect(arr).to.deep.equal(["apple", "banana", "grape", "orange"]);
   });
 });
