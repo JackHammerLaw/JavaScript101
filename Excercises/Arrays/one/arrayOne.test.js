b60478fbf11a3166ffa78e77bf72859ac73d3a4b
Dan Mcgillicuddy <danmcgillicuddy@Dans-MBP.fios-router.home>
2020-06-16 12:20:27 -0400

    fix: variable in test

--- Excercises/Arrays/one/arrayOne.test.js
+++ Excercises/Arrays/one/arrayOne.test.js
@@ -17,7 +17,7 @@ describe("Array One", function () {
     expect(arr).to.deep.equal(["apple", "banana", "grape", "orange"]);
   });
   it("Remove correct element", function () {
-    const arr = insertElementAtIndex();
+    const arr = removeElementAtIndex();
     expect(arr).to.have.lengthOf(3);
     expect(arr).to.deep.equal(["toyota", "honda", "hummer"]);
   });
